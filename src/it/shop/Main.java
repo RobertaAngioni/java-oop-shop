11fb5b9c262fd92d3348793f1a7c0eb5bea653fa
RobertaAngioni <robertaangioni92@gmail.com>
2021-11-25 15:14:20 +0100

    primo commit

---
+++ src/it/shop/Main.java
@@ -0,0 +1,12 @@
+package it.shop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Prodotto prod1 = new Prodotto();
+		System.out.println("Il nome del codice numero " + prod1.getCode() + " è " + prod1.getName());
+	}
+
+}
---
+++ src/it/shop/Prodotto.java
@@ -0,0 +1,110 @@
+package it.shop;
+
+import java.util.Random;
+
+public class Prodotto {
+	
+	// attributi
+	
+	private int code;
+	private String name;
+	private String description;
+	private int price;
+	private int iva;
+	
+	// cpstruttori
+	
+	public Prodotto() {
+		this.code = codeGenerator();
+		this.price = basePrice();
+		this.iva = ivaPriceCalculator();
+		this.name = extendedName();
+	}
+	
+	
+	
+	
+	// metodi
+	
+	public String getName() {
+		return name;
+	}
+
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+
+
+	public int getIva() {
+		return iva;
+	}
+
+
+
+
+	public void setIva(int iva) {
+		this.iva = iva;
+	}
+
+
+
+
+	public int getCode() {
+		return code;
+	}
+
+
+
+
+	private int codeGenerator() {
+		Random rand = new Random();
+		return rand.nextInt();
+	}
+	
+	public int basePrice() {
+		return price;
+	}
+	
+	public int ivaPriceCalculator() {
+		return price + iva;
+	}
+	
+	private String extendedName() {
+		return code + name;
+	}
+	
+
+}
